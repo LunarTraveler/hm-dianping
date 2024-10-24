bdd0fd6c3499920e63bbf0cfa45a292d19f3bbbd
LunarTraveler <2961478685@qq.com>
2024-10-24 18:10:44 +0800

    使用lua脚本完成redis操作是原子操作

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -17,6 +20,13 @@ public class SimpleRedisLock implements ILock {
 
     public static final String KEY_PREFIX = "lock:";
     public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    public static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        // classPath就是对应于resources这个目录的
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeout) {
@@ -31,14 +41,21 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        // 获取线程表示
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的表示
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // lua脚本实现redis的操作是原子性的
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unlock() {
+//        // 获取线程表示
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        // 获取锁中的表示
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            // 释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 
 }
---
+++ src/main/resources/unlock.lua
@@ -0,0 +1,6 @@
+-- 比较判断
+if (redis.call('get', KEYS[1]) == ARGV[1]) then
+    -- 释放锁
+    return redis.call('del', KEYS[1])
+end
+    return 0
